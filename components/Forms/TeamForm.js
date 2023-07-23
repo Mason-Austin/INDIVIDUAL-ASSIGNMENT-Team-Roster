71cd815ba538be151d3810324b9f630cabd9d729
Mason Austin <mason47austin@gmail.com>
2023-07-22 20:11:08 -0500

    finished TeamForm

--- components/Forms/TeamForm.js
+++ components/Forms/TeamForm.js
@@ -1,9 +1,11 @@
-// import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { FloatingLabel } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { useAuth } from '../../utils/context/authContext';
+import { createTeam, updateTeam } from '../../api/teamData';
 
 const initialState = {
   game: '',
@@ -11,29 +13,62 @@ const initialState = {
 };
 
 function TeamForm({ obj }) {
-  // const [formInput, SetFormInput] = useState(initialState);
-  // const router = useRouter();
-  const { user } = useAuth;
+  const [formInput, SetFormInput] = useState(initialState);
+  const router = useRouter();
+  const { user } = useAuth();
 
   useEffect(() => {
-    // if (obj.firebaseKey) SetFormInput(obj);
+    if (obj.firebaseKey) SetFormInput(obj);
   }, [obj, user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    SetFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.warn(user);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (obj.firebaseKey) {
+      updateTeam(formInput).then(() => router.push(`/team/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createTeam(payload).then(({ name }) => {
+        const patchPayLoad = { firebaseKey: name };
+        updateTeam(patchPayLoad).then(() => {
+          router.push('/');
+        });
+      });
+    }
   };
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicInput">
-        <Form.Label>Team Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter Team Name" />
-      </Form.Group>
+      <FloatingLabel controlId="floatingInput2" label="Team Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Team Name"
+          name="name"
+          value={formInput.name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
 
-      <Form.Group className="mb-3" controlId="formBasicInput">
-        <Form.Label>Game Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter Game Name" />
-      </Form.Group>
+      <FloatingLabel controlId="floatingInput2" label="Game" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Game Name"
+          name="game"
+          value={formInput.game}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
       <Button variant="primary" type="submit">
         Submit
       </Button>
